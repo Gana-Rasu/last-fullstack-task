966a99f37500691e0abcfdd11a1f83998ac5f501
Gana <gananathanthirunavukkarasu@gmail.com>
2022-08-15 14:32:12 +0530

    last task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TextField id="outlined-basic" label="Name" variant="outlined" /><br/>
+      <TextField id="outlined-basic" label="Mobile" variant="outlined" /><br/>
+      <TextField id="outlined-basic" label="Age" variant="outlined" /><br/>
+      <TextField id="outlined-basic" label="Country" variant="outlined" /><br/>
+      <TextField id="outlined-basic" label="State" variant="outlined" /><br/>
+      <TextField id="outlined-basic" label="City" variant="outlined" /><br/>
+      <Button variant="contained" >SUBMIT</Button>
     </div>
   );
 }
-
-export default App;
